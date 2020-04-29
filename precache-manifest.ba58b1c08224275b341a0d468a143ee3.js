aa8bf886e541400ce327218079698e9ef62b5b40
minhvan251 <minhvan251@gmail.com>
2020-04-29 00:56:40 -0400

    Updates

---
+++ precache-manifest.ba58b1c08224275b341a0d468a143ee3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "81f58a29e4d433b46634b07040776537",
+    "url": "/monster_project/index.html"
+  },
+  {
+    "revision": "d8ae7eea3ffb3cefeb16",
+    "url": "/monster_project/static/css/main.43b399ea.chunk.css"
+  },
+  {
+    "revision": "b66b835617ced474408f",
+    "url": "/monster_project/static/js/2.b7a674f6.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monster_project/static/js/2.b7a674f6.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "d8ae7eea3ffb3cefeb16",
+    "url": "/monster_project/static/js/main.405f5036.chunk.js"
+  },
+  {
+    "revision": "35a83f1d1a114362ea18",
+    "url": "/monster_project/static/js/runtime-main.13b200d7.js"
+  }
+]);
